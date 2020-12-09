4f732dd4393113a3d1c9a93b0fc00056b3f6d241
Yordan Rashkov <jrashkov@gmail.com>
2020-12-09 14:04:07 +0200

    Add ComplexDataModel

--- ContosoUniversity/Models/Enrollment.cs
+++ ContosoUniversity/Models/Enrollment.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -18,6 +19,7 @@ namespace ContosoUniversity.Models
 
         public int StudentID { get; set; }
 
+        [DisplayFormat(NullDisplayText = "No grade")]
         public Grade? Grade { get; set; }
 
 
